dac543d0eb4bc653aad2c5f8386637b39c61f6db
Joseph Holten <joseph@holten.com>
2024-06-28 18:34:20 +0200

    basic pointer chasing working

--- profile/profile.cpp
+++ profile/profile.cpp
@@ -1,18 +1,49 @@
 #include "docopt.cpp/docopt.h"
 #include "nlohmann/json.hpp"
 #include <chrono>
-#include "fcntl.h"
-#include "unistd.h"
-#include "stdio.h"
-#include "stdint.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdint.h>
 
 using json = nlohmann::json;
+
+// serialize in big-endian
+size_t serialize(uint64_t x, uint8_t* p_buf) {
+    size_t number_of_bytes_written = sizeof(x);
+    p_buf[0] = (x >> 56) & 0xFF;
+    p_buf[1] = (x >> 48) & 0xFF;
+    p_buf[2] = (x >> 40) & 0xFF;
+    p_buf[3] = (x >> 32) & 0xFF;
+
+    p_buf[4] = (x >> 24) & 0xFF;
+    p_buf[5] = (x >> 16) & 0xFF;
+    p_buf[6] = (x >>  8) & 0xFF;
+    p_buf[7] = (x >>  0) & 0xFF;
+    return number_of_bytes_written;
+}
+
+// deserialize in big endian
+uint64_t deserialize(uint8_t* p_buf) {
+    uint64_t x = 0;
+    x |= ((uint64_t)p_buf[7] <<  0);
+    x |= ((uint64_t)p_buf[6] <<  8);
+    x |= ((uint64_t)p_buf[5] << 16);
+    x |= ((uint64_t)p_buf[4] << 24);
+
+    x |= ((uint64_t)p_buf[3] << 32);
+    x |= ((uint64_t)p_buf[2] << 40);
+    x |= ((uint64_t)p_buf[1] << 48);
+    x |= ((uint64_t)p_buf[0] << 56);
+    return x;
+}
 
 static const char USAGE[] =
 R"(profile
 
 Usage:
     profile read <data_path> [--json]
+    profile cache <data_path> [--fill] [--json]
 )";
 
 void profile_read(std::string data_path, bool json_logging) {
@@ -34,6 +65,7 @@ void profile_read(std::string data_path, bool json_logging) {
         if (file_size < 0)
         {
             perror("file_size");
+            close(fd);
             return;
         }
         // reset to start
@@ -90,10 +122,80 @@ void profile_read(std::string data_path, bool json_logging) {
     close(fd);
 }
 
+void profile_cache(std::string data_path, bool fill, bool json_logging) {
+    (void)json_logging;
+    static const size_t block_size = 8192;
+    static const size_t initial_size = 8 * block_size;
+    static const size_t max_size = initial_size;
+
+    static const size_t iterations = 1;
+
+    int fd = open(data_path.c_str(), O_RDWR | O_CREAT);
+    if (fd < 0) {
+        perror("Error: open");
+        return;
+    }
+
+    for (size_t size = initial_size; size <= max_size; size *= 2) {
+        printf("size: %ld\n", size);
+        if (fill) {
+            for (size_t off = lseek64(fd, 0, SEEK_END); off < size; off += block_size) {
+                uint8_t block[block_size] = {0};
+                ssize_t bytes = write(fd, block, block_size);
+                if (bytes < 0 || (size_t)bytes < block_size) {
+                    perror("Error: fill");
+                    goto cleanup;
+                }
+            }
+        }
+
+        lseek64(fd, 0, SEEK_SET);
+        for (size_t stride = block_size; stride < size; stride *= 2) {
+            printf("stride: %ld\n", stride);
+
+            // setup closed loop
+            for (size_t idx = 0; idx < size; idx += stride) {
+                uint8_t buf[sizeof(idx)];
+                lseek64(fd, idx, SEEK_SET);
+                serialize((idx + block_size) % size, buf);
+                write(fd, buf, sizeof(idx));
+            }
+
+            for (size_t iteration = 0; iteration < iterations; iteration++) {
+                volatile size_t idx = 0;
+                do {
+                    printf("  %ld\n", idx);
+
+                    uint8_t block[block_size];
+                    lseek64(fd, idx, SEEK_SET);
+                    ssize_t bytes = read(fd, block, block_size);
+
+                    if (bytes < 0) {
+                        perror("Error: reading block");
+                        goto cleanup;
+                    } else if ((size_t)bytes < sizeof(idx)) {
+                        fprintf(stderr, "Error: could only read %ld bytes, not enough to deserialize next idx\n", bytes);
+                        goto cleanup;
+                    }
+                    idx = deserialize(block);
+                } while(idx != 0);
+            }
+        }
+    }
+
+cleanup:
+    close(fd);
+    remove(data_path.c_str());
+}
+
 int main(int argc, char** argv) {
     std::map<std::string, docopt::value> args = docopt::docopt(USAGE, {argv + 1, argv + argc});
 
-    if (args["read"]) {
+    if (args["read"].asBool()) {
         profile_read(args["<data_path>"].asString(), args["--json"].asBool());
+    }
+
+    else if (args["cache"].asBool()) {
+        profile_cache(args["<data_path>"].asString(), args["--fill"].asBool(), args["--json"].asBool());
     }
 }
