af3f6cb73a5d184171585b333fbfccefcf112a4b
Joseph Holten <joseph@holten.com>
2024-09-11 15:14:12 -0400

    first draft at conan package management

---
+++ conanfile.py
@@ -0,0 +1,57 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+from conan.tools.scm import Git
+
+import re
+
+class jlib(ConanFile):
+    name = "kleardb"
+
+    # Optional metadata
+    author = "Joseph Holten & Max Hirsch (joseph@holten.com, max.levin.hirsch@icloud.com)"
+    description = "A SQL database implementation"
+
+    requires = "fmt/[>=11]", "gtest/[>=1.15]"
+
+    def set_version(self):
+        git = Git(self)
+
+        diff = git.run("status --short")
+        if diff != "":
+            self.output.warning("working tree is not clean")
+            #exit(1)
+
+        descr = git.run("describe --tags")
+        regex = r"^(\d+\.\d+\.\d+(?:-pre)?)(?:-(\d+)-((?:\d|\w)+))?$"
+        groups = re.findall(regex, descr)
+        if len(groups) != 1:
+            self.output.error(f"unknown tag format '{descr}'")
+            exit(1)
+        else:
+            group = groups[0]
+            tag, commits, h = group
+            if commits == "" and h == "":
+                self.version = descr
+            else:
+                if "-pre" not in tag:
+                    self.output.error(f"commit {descr} is after tag {tag} but has no '-pre' in current tag")
+                    exit(1)
+                version = f"{tag}.{commits}+{h}"
+                self.output.highlight(f"version = {version}")
+                self.version = version
+
+    #def layout(self):
+    #    cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
